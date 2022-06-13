d6fb69c4e9670510756ddd02e7046c762ad31d38
Konstantin Moskvin <mr.koctia@gmail.com>
2022-06-13 15:48:13 +0300

    feat: added burger functionality

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-$(document).ready(function() {
-   $('.header__burger').click(function() {
-      $('.header__burger,.header__nav,.main__search-parameters').toggleClass('active');
-      $('body').toggleClass('lock');
-   });
+document.addEventListener('DOMContentLoaded', function(e){
+  let button = document.querySelector('.header__burger');
+
+  button.onclick = () => {
+    document.querySelector('.header__burger').classList.toggle('active');
+    document.querySelector('.header__nav').classList.toggle('active');
+    document.querySelector('body').classList.toggle('lock');
+  }
+
+  let item = document.querySelectorAll('.nav__item');
+  for (var items of item) {
+    items.onclick = () => {
+      document.querySelector('.header__burger').classList.remove('active');
+      document.querySelector('.header__nav').classList.remove('active');
+      document.querySelector('body').classList.remove('lock');
+      location.reload();
+    };
+  }
 });
